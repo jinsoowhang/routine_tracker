8c5fdf8d105997c53b111ed3d5a3ad169c91358c
jwtreones <jwtreones@gmail.com>
2025-01-03 14:33:15 -0800

    Add data model and visualization for lifting weight repetitions

--- dbt/models/mart/fct__gym_exercises.sql
+++ dbt/models/mart/fct__gym_exercises.sql
@@ -2,7 +2,8 @@ WITH raw AS (
     SELECT 
         TO_DATE(gym_date::TEXT, 'YYYYMMDD') AS gym_date,
         TRIM(attribute_1) AS gym_exercise_type,
-        TRIM(attribute_2) AS gym_exercise_weight
+        TRIM(attribute_2) AS gym_exercise_weight,
+        attribute_3 AS gym_exercise_repetitions
     FROM {{ ref('stg__gym') }}  -- Reference the staging model
     WHERE attribute_6 = 'gym'
 ),
@@ -11,7 +12,8 @@ results AS (
 	SELECT 
         gym_date,
 		gym_exercise_type,
-		gym_exercise_weight
+		gym_exercise_weight,
+        gym_exercise_repetitions
 	FROM raw
 )
 
