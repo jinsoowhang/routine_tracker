0834e818f00e2d019aa5edfa788bbdb87fb81250
jinsoowhang <jwtreones@gmail.com>
2025-05-03 13:11:21 -0700

    add more food historical data

--- dbt/models/staging/stg__food.sql
+++ dbt/models/staging/stg__food.sql
@@ -2,21 +2,27 @@
 WITH source AS (
     SELECT 
         TO_DATE(rhythm_date::TEXT, 'YYYYMMDD') AS rhythm_date,
+        attribute_1 as food_activity,
+        attribute_2 as meal_of_day,
         CASE 
             WHEN notes ~ 'food:\s*"([^"]+)"' 
             THEN REGEXP_REPLACE(notes, '.*food:\s*"([^"]+)".*', '\1', 'i')
-            ELSE NULL
+            ELSE notes
         END AS food,
         CASE 
             WHEN notes ~ 'restaurant:\s*"([^"]+)"' 
             THEN REGEXP_REPLACE(notes, '.*restaurant:\s*"([^"]+)".*', '\1', 'i')
-            ELSE NULL
+            ELSE places
         END AS restaurant
     FROM {{ ref('stg__rhythm') }}  -- Reference the staging model
+    WHERE attribute_1 in ('eat', 'cook')
+      AND notes is not null
 )
 
 SELECT
     rhythm_date,
+    food_activity,
+    meal_of_day,
     food,
     restaurant
 FROM source
